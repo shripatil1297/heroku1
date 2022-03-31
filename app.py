e6d63119a4b15809be216b709f8e6311a956ab9f
bhagyashri subhash patil <96486176+shripatil1297@users.noreply.github.com>
2022-04-01 05:13:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 
+USERNAME_PASSWORD_PAIRS=[
+  ['guvi','guvi'],['shree','shree]
+  ]
+  app=dash.Dash()
+  auth=dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
+  server = app.server
 
 app = dash.Dash(__name__)
-server = app.server
+
 
 df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 
